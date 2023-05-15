05f75f5aa6c5e9694bcfa9eb8c15f5198d99afa8
Yohei Shimmyo <qwpmb554@gmail.com>
2023-05-15 18:22:47 +0900

    install dependencies by automake outside of chroot

--- contrib/scripts/install.bash
+++ contrib/scripts/install.bash
@@ -138,10 +138,7 @@ rm $MOUNT_POINT/home/pi/.config/user-dirs.locale # disable initial check of loca
 ## TODO: summarize dependencies into "control" in a deb package with contesnts of obj (${OBJDIR})and here apt should call that package.
 chroot $MOUNT_POINT apt install -y \
 fcitx-mozc i2c-tools open-jtalk open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001 build-essential zlib1g-dev libsdl2-dev libasound2-dev dnsutils nmap telnet nkf lirc fswebcam gimp vlc tuxtype ruby libgtk2.0-dev libglew-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libgles2-mesa-dev libegl1-mesa-dev open-jtalk open-jtalk-mecab-naist-jdic
-# TODO: `make install` should not run under chroot; otherwise, when the host owns gawk, automake sets AWK=gawk but Raspberry Pi OS does not have gawk but awk.
-# A workaround could be to set AWK=awk but the other dependencies will potentially be broken.
-chroot $MOUNT_POINT su -c "make -C $OBJDIR_EMU AWK=awk install"
-chown -R 1000:1000 $MOUNT_POINT/home/pi/ome
+make DESTDIR=$MOUNT_POINT/usr/local install
 
 # Remove the initial wizard and change pw into `raspberry`
 ## /usr/lib/userconf-pi/userconf calls /usr/bin/cancel-rename and then
